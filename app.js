64f96ebf06aaca6fecceb2cbc4e3868e0b5b74e1
王志龙 <zhilong.wang@ele.me>
2016-04-06 19:49:42 +0800

    alpha version will be complete in days!

--- app.js
+++ app.js
@@ -8,37 +8,46 @@ var pathHandler = require('./pathHandler');
 var http = require('http');
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer({});
+var url = require('url');
 
-app.get('/v1/carts/:sid/addresses', function(req, res, next){
-    var addresses = fileObjectReader('addresses', function(data){
-        if (data) {
-            res.contentType("application/json");
-            res.send(JSON.stringify(data));
-        }
-        else {
-            next();
-        }
-    });
+//app.get('/v1/carts/:sid/addresses', function(req, res){
+//    var addresses = fileObjectReader('addresses', function(data){
+//        if (data) {
+//            res.contentType("application/json");
+//            res.send(JSON.stringify(data));
+//        }
+//        else {
+//            res.error("not found");
+//        }
+//    });
+//});
+
+app.all('*', function(req, res, next){
+    console.log(req.path);
+    next();
 });
 
 app.use(function(req, res, next) {
-    console.log(req.originalUrl);
-    var sreq = http.request({
-        host:     'restapi.elenet.me', // 目标主机
-        path:     req.originalUrl,
-        method:   req.method, // 请求方式
-        headers:  req.headers
-    }, function(sres){
+    var u = url.parse(req.url);
+    var options = {
+        host: 'restapi.elenet.me',
+        port: u.port || 80,
+        path: u.path,
+        method: req.method,
+        headers: {
+            'cookie':req.header('cookie'),
+            'User-Agent': req.header('User-Agent') || '',
+            'X-DeviceInfo': req.header('X-DeviceInfo') || ''
+        }
+    };
+    var sreq = http.request(options, function(sres){
         res.writeHead(sres.statusCode,sres.headers);
         sres.pipe(res);
-        sres.on('end', function(){
-            console.log(req.originalUrl + 'done');
-        });
     });
     req.pipe(sreq);
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(80, function(){
     var host = server.address().address;
     var port = server.address().port;
     console.log("mock server is running on "+ host + ":"+port);
